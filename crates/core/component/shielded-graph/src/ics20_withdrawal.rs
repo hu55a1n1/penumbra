fc812dbd51a816251f3efa0595efd6d589f3368e
Shoaib Ahmed <sufialhussaini@gmail.com>
2024-12-07 00:23:28 +0400

    Duplicate shielded-pool component as shielded-graph

---
+++ crates/core/component/shielded-graph/src/ics20_withdrawal.rs
@@ -0,0 +1,170 @@
+use ibc_types::core::{channel::ChannelId, channel::PortId, client::Height as IbcHeight};
+use penumbra_asset::{
+    asset::{self, Metadata},
+    Balance, Value,
+};
+use penumbra_keys::Address;
+use penumbra_num::Amount;
+use penumbra_proto::{
+    penumbra::core::component::ibc::v1::{self as pb, FungibleTokenPacketData},
+    DomainType,
+};
+use penumbra_txhash::{EffectHash, EffectingData};
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+
+#[cfg(feature = "component")]
+use penumbra_ibc::component::packet::{IBCPacket, Unchecked};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(try_from = "pb::Ics20Withdrawal", into = "pb::Ics20Withdrawal")]
+pub struct Ics20Withdrawal {
+    // a transparent value consisting of an amount and a denom.
+    pub amount: Amount,
+    pub denom: asset::Metadata,
+    // the address on the destination chain to send the transfer to
+    pub destination_chain_address: String,
+    // a "sender" penumbra address to use to return funds from this withdrawal.
+    // this should be an ephemeral address
+    pub return_address: Address,
+    // the height (on Penumbra) at which this transfer expires (and funds are sent
+    // back to the return address?). NOTE: if funds are sent back to the sender,
+    // we MUST verify a nonexistence proof before accepting the timeout, to
+    // prevent relayer censorship attacks. The core IBC implementation does this
+    // in its handling of validation of timeouts.
+    pub timeout_height: IbcHeight,
+    // the timestamp at which this transfer expires, in nanoseconds after unix epoch.
+    pub timeout_time: u64,
+    // the source channel used for the withdrawal
+    pub source_channel: ChannelId,
+
+    // Whether to use a "compat" (bech32, non-m) address for the return address in the withdrawal,
+    // for compatability with chains that expect to be able to parse the return address as bech32.
+    pub use_compat_address: bool,
+}
+
+#[cfg(feature = "component")]
+impl From<Ics20Withdrawal> for IBCPacket<Unchecked> {
+    fn from(withdrawal: Ics20Withdrawal) -> Self {
+        Self::new(
+            PortId::transfer(),
+            withdrawal.source_channel.clone(),
+            withdrawal.timeout_height,
+            withdrawal.timeout_time,
+            withdrawal.packet_data(),
+        )
+    }
+}
+
+impl Ics20Withdrawal {
+    pub fn value(&self) -> Value {
+        Value {
+            amount: self.amount,
+            asset_id: self.denom.id(),
+        }
+    }
+
+    pub fn balance(&self) -> Balance {
+        -Balance::from(self.value())
+    }
+
+    pub fn packet_data(&self) -> Vec<u8> {
+        let ftpd: FungibleTokenPacketData = self.clone().into();
+
+        // In violation of the ICS20 spec, ibc-go encodes transfer packets as JSON.
+        serde_json::to_vec(&ftpd).expect("can serialize FungibleTokenPacketData as JSON")
+    }
+
+    // stateless validation of an Ics20 withdrawal action.
+    pub fn validate(&self) -> anyhow::Result<()> {
+        if self.timeout_time == 0 {
+            anyhow::bail!("timeout time must be non-zero");
+        }
+
+        // in order to prevent clients from inadvertantly identifying themselves by their clock
+        // skew, enforce that timeout time is rounded to the nearest minute
+        if self.timeout_time % 60_000_000_000 != 0 {
+            anyhow::bail!(
+                "withdrawal timeout timestamp {} is not rounded to one minute",
+                self.timeout_time
+            );
+        }
+
+        // NOTE: we could validate the destination chain address as bech32 to prevent mistyped
+        // addresses, but this would preclude sending to chains that don't use bech32 addresses.
+
+        Ok(())
+    }
+}
+
+impl EffectingData for Ics20Withdrawal {
+    fn effect_hash(&self) -> EffectHash {
+        EffectHash::from_proto_effecting_data(&self.to_proto())
+    }
+}
+
+impl DomainType for Ics20Withdrawal {
+    type Proto = pb::Ics20Withdrawal;
+}
+
+impl From<Ics20Withdrawal> for pb::Ics20Withdrawal {
+    fn from(w: Ics20Withdrawal) -> Self {
+        pb::Ics20Withdrawal {
+            amount: Some(w.amount.into()),
+            denom: Some(w.denom.base_denom().into()),
+            destination_chain_address: w.destination_chain_address,
+            return_address: Some(w.return_address.into()),
+            timeout_height: Some(w.timeout_height.into()),
+            timeout_time: w.timeout_time,
+            source_channel: w.source_channel.to_string(),
+            use_compat_address: w.use_compat_address,
+        }
+    }
+}
+
+impl TryFrom<pb::Ics20Withdrawal> for Ics20Withdrawal {
+    type Error = anyhow::Error;
+    fn try_from(s: pb::Ics20Withdrawal) -> Result<Self, Self::Error> {
+        Ok(Self {
+            amount: s
+                .amount
+                .ok_or_else(|| anyhow::anyhow!("missing amount"))?
+                .try_into()?,
+            denom: Metadata::default_for(
+                &s.denom
+                    .ok_or_else(|| anyhow::anyhow!("missing denom metadata"))?
+                    .try_into()?,
+            )
+            .ok_or_else(|| anyhow::anyhow!("could not generate default denom metadata"))?,
+            destination_chain_address: s.destination_chain_address,
+            return_address: s
+                .return_address
+                .ok_or_else(|| anyhow::anyhow!("missing sender"))?
+                .try_into()?,
+            timeout_height: s
+                .timeout_height
+                .ok_or_else(|| anyhow::anyhow!("missing timeout height"))?
+                .try_into()?,
+            timeout_time: s.timeout_time,
+            source_channel: ChannelId::from_str(&s.source_channel)?,
+            use_compat_address: s.use_compat_address,
+        })
+    }
+}
+
+impl From<Ics20Withdrawal> for pb::FungibleTokenPacketData {
+    fn from(w: Ics20Withdrawal) -> Self {
+        let return_address = match w.use_compat_address {
+            true => w.return_address.compat_encoding(),
+            false => w.return_address.to_string(),
+        };
+
+        pb::FungibleTokenPacketData {
+            amount: w.value().amount.to_string(),
+            denom: w.denom.to_string(),
+            receiver: w.destination_chain_address,
+            sender: return_address,
+            memo: "".to_string(),
+        }
+    }
+}
---
+++ crates/core/component/shielded-graph/src/spend/action.rs
@@ -0,0 +1,126 @@
+use std::convert::{TryFrom, TryInto};
+
+use anyhow::{Context, Error};
+use decaf377_rdsa::{Signature, SpendAuth, VerificationKey};
+use penumbra_asset::balance;
+use penumbra_proto::{core::component::shielded_pool::v1 as pb, DomainType};
+use penumbra_sct::Nullifier;
+use penumbra_txhash::{EffectHash, EffectingData};
+use serde::{Deserialize, Serialize};
+
+use crate::SpendProof;
+
+#[derive(Clone, Debug)]
+pub struct Spend {
+    pub body: Body,
+    pub auth_sig: Signature<SpendAuth>,
+    pub proof: SpendProof,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(try_from = "pb::SpendBody", into = "pb::SpendBody")]
+pub struct Body {
+    pub balance_commitment: balance::Commitment,
+    pub nullifier: Nullifier,
+    pub rk: VerificationKey<SpendAuth>,
+}
+
+impl EffectingData for Body {
+    fn effect_hash(&self) -> EffectHash {
+        EffectHash::from_proto_effecting_data(&self.to_proto())
+    }
+}
+
+impl EffectingData for Spend {
+    fn effect_hash(&self) -> EffectHash {
+        // The effecting data is in the body of the spend, so we can
+        // just use hash the proto-encoding of the body.
+        self.body.effect_hash()
+    }
+}
+
+impl DomainType for Spend {
+    type Proto = pb::Spend;
+}
+
+impl From<Spend> for pb::Spend {
+    fn from(msg: Spend) -> Self {
+        pb::Spend {
+            body: Some(msg.body.into()),
+            auth_sig: Some(msg.auth_sig.into()),
+            proof: Some(msg.proof.into()),
+        }
+    }
+}
+
+impl TryFrom<pb::Spend> for Spend {
+    type Error = Error;
+
+    fn try_from(proto: pb::Spend) -> anyhow::Result<Self, Self::Error> {
+        let body = proto
+            .body
+            .ok_or_else(|| anyhow::anyhow!("missing spend body"))?
+            .try_into()
+            .context("malformed spend body")?;
+        let auth_sig = proto
+            .auth_sig
+            .ok_or_else(|| anyhow::anyhow!("missing auth sig"))?
+            .try_into()
+            .context("malformed auth sig")?;
+        let proof = proto
+            .proof
+            .ok_or_else(|| anyhow::anyhow!("missing proof"))?
+            .try_into()
+            .context("malformed spend proof")?;
+
+        Ok(Spend {
+            body,
+            auth_sig,
+            proof,
+        })
+    }
+}
+
+impl DomainType for Body {
+    type Proto = pb::SpendBody;
+}
+
+impl From<Body> for pb::SpendBody {
+    fn from(msg: Body) -> Self {
+        pb::SpendBody {
+            balance_commitment: Some(msg.balance_commitment.into()),
+            nullifier: Some(msg.nullifier.into()),
+            rk: Some(msg.rk.into()),
+        }
+    }
+}
+
+impl TryFrom<pb::SpendBody> for Body {
+    type Error = Error;
+
+    fn try_from(proto: pb::SpendBody) -> anyhow::Result<Self, Self::Error> {
+        let balance_commitment: balance::Commitment = proto
+            .balance_commitment
+            .ok_or_else(|| anyhow::anyhow!("missing balance commitment"))?
+            .try_into()
+            .context("malformed balance commitment")?;
+
+        let nullifier = proto
+            .nullifier
+            .ok_or_else(|| anyhow::anyhow!("missing nullifier"))?
+            .try_into()
+            .context("malformed nullifier")?;
+
+        let rk = proto
+            .rk
+            .ok_or_else(|| anyhow::anyhow!("missing rk"))?
+            .try_into()
+            .context("malformed rk")?;
+
+        Ok(Body {
+            balance_commitment,
+            nullifier,
+            rk,
+        })
+    }
+}
